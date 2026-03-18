db9ee6ebb33097659d64aee5486d38f9d1ba5270
Noah Zarro <zarro@ubique.ch>
2026-03-18 22:56:17 +0100

    added gitignore

--- app/src/main/java/ch/noah/soundboard/networking/SoundBoardDto.kt
+++ app/src/main/java/ch/noah/soundboard/networking/SoundBoardDto.kt
@@ -1,6 +1,15 @@
 package ch.noah.soundboard.networking
 
 data class SoundboadDto(
-    val title: String,
-    val soundboardItems: List<SoundboadItemDto>,
-)
+	val title: String,
+	val version: String,
+	val soundboardItems: List<SoundboadItemDto>,
+) {
+	fun isNewerThan(other: SoundboadDto): Boolean {
+		val major = version.substringBefore(".").toIntOrNull() ?: 0
+		val minor = version.substringAfter(".").toIntOrNull() ?: 0
+		val otherMajor = other.version.substringBefore(".").toIntOrNull() ?: 0
+		val otherMinor = other.version.substringAfter(".").toIntOrNull() ?: 0
+		return major > otherMajor || (major == otherMajor && minor > otherMinor)
+	}
+}
